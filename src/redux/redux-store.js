c850bfbe1da7610b7463e57f5079d9ad167c748f
Kross55 <iyakushenko84@gmail.com>
2022-06-20 20:45:07 +0300

    redux-store

---
+++ src/redux/redux-store.js
@@ -0,0 +1,14 @@
+import {combineReducers, legacy_createStore as createStore} from "redux";
+import profileReducer from './profileReduser';
+import dialogsReducer from "./dialogsReduser";
+import sidebarReducer from "./sidebarReduser";
+
+let reducers = combineReducers ({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar: sidebarReducer,
+});
+
+let store = createStore(reducers);
+
+export default store;
