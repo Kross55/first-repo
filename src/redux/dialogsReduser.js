1061dd9fee9c1fc79c2302d219972f407cda14dc
Kross55 <iyakushenko84@gmail.com>
2022-06-24 09:02:39 +0300

    lesson-48-reducers-pureCode

--- src/redux/dialogsReduser.js
+++ src/redux/dialogsReduser.js
@@ -2,47 +2,43 @@ const UPDATE_NEW_MESSAGE_BODY = "UPDATE_NEW_MESSAGE_BODY";
 const SEND_MESSAGE = "SEND_MESSAGE";
 
 let initialeState = {
-        dialogs: [
-            {id: 1, name: 'Leo'},
-            {id: 2, name: 'Dony'},
-            {id: 3, name: 'Miky'},
-            {id: 4, name: 'Raf'},
-            {id: 5, name: 'Splinter'},
-            {id: 6, name: 'April'},
-            {id: 7, name: 'Shreder'},
-        ],
-        messages: [
-            {id: 1, message: 'Hi everyone'},
-            {id: 2, message: 'Yo'},
-            {id: 3, message: 'What am i doing here?'},
-            {id: 4, message: 'Yok'},
-            {id: 5, message: 'Hey'},
-        ],
-        newMessageBody: '',
-        answers: [
-            {id: 1, answer: 'Hey you'},
-            {id: 2, answer: 'Go fuck yourself'},
-            {id: 3, answer: 'I love you guys!'},
-        ]
+  dialogs: [
+    { id: 1, name: 'Leo' },
+    { id: 2, name: 'Dony' },
+    { id: 3, name: 'Miky' },
+    { id: 4, name: 'Raf' },
+    { id: 5, name: 'Splinter' },
+    { id: 6, name: 'April' },
+    { id: 7, name: 'Shreder' },
+  ],
+  messages: [
+    { id: 1, message: 'Hi everyone' },
+    { id: 2, message: 'Yo' },
+    { id: 3, message: 'What am i doing here?' },
+    { id: 4, message: 'Yok' },
+    { id: 5, message: 'Hey' },
+  ],
+  newMessageBody: '',
+  answers: [
+    { id: 1, answer: 'Hey you' },
+    { id: 2, answer: 'Go fuck yourself' },
+    { id: 3, answer: 'I love you guys!' },
+  ]
 };
 
-const dialogsReducer = (state=initialeState, action) => {
+const dialogsReducer = (state = initialeState, action) => {
   switch (action.type) {
-    case UPDATE_NEW_MESSAGE_BODY: {
-      let stateCopy = {...state};
-      stateCopy.newMessageBody = action.body;
-      return stateCopy;
-    }
-    case SEND_MESSAGE: {
-      let stateCopy = JSON.parse(JSON.stringify(state));
-      let newMessage = {
-        id: 6,
-        message: state.newMessageBody,
+    case UPDATE_NEW_MESSAGE_BODY:
+      return {
+        ...state,
+        newMessageBody: action.body
       };
-      stateCopy.messages.push(newMessage);
-      stateCopy.newMessageBody = "";
-      return stateCopy;
-    }
+    case SEND_MESSAGE:
+      return {
+        ...state,
+        messages: [...state.messages, { id: 6, message: state.newMessageBody }],
+        newMessageBody: '',
+      }
     default:
       return state;
   }
--- src/redux/profileReduser.js
+++ src/redux/profileReduser.js
@@ -2,41 +2,41 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const ADD_POST = 'ADD-POST';
 
 let initialeState = {
-        posts: [
-            {id: 1, message: 'Hi everyone', likes: 13},
-            {id: 2, message: 'Hi, my friends!', likes: 40},
-            {id: 3, message: 'What am i doing here?', likes: 61},
-            {id: 4, message: 'Oops', likes: 17},
-            {id: 5, message: 'Hey', likes: 93},
-        ],
-        newPostText: 'Yo',
+    posts: [
+        { id: 1, message: 'Hi everyone', likes: 13 },
+        { id: 2, message: 'Hi, my friends!', likes: 40 },
+        { id: 3, message: 'What am i doing here?', likes: 61 },
+        { id: 4, message: 'Oops', likes: 17 },
+        { id: 5, message: 'Hey', likes: 93 },
+    ],
+    newPostText: 'Yo',
 };
 
-const profileReducer =(state = initialeState, action) => {
+const profileReducer = (state = initialeState, action) => {
     switch (action.type) {
-        case UPDATE_NEW_POST_TEXT: {
-            let stateCopy = {...state}
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
-        };
-        case ADD_POST: {
-            let stateCopy = JSON.parse(JSON.stringify(state))
+        case UPDATE_NEW_POST_TEXT:
+            return {
+                ...state,
+                newPostText: action.newText
+            };
+        case ADD_POST:
             let newPost = {
                 id: 6,
                 message: state.newPostText,
                 likes: 0,
             };
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
-        };
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            };
         default:
             return state;
     }
 };
 
 export const updateNewPostTextActionCreator = (text) =>
-    ({type: UPDATE_NEW_POST_TEXT, newText: text});
-export const addPostActionCreator = () => ({type: ADD_POST});
+    ({ type: UPDATE_NEW_POST_TEXT, newText: text });
+export const addPostActionCreator = () => ({ type: ADD_POST });
 
 export default profileReducer;
