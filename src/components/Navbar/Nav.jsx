243073133511738b41dbabe1f8024d1326892457
Kross55 <iyakushenko84@gmail.com>
2022-06-26 18:57:16 +0300

    lesson-51-axios-Users-API

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -4,7 +4,6 @@ import {NavLink} from "react-router-dom";
 import FriendItem from "./Friends/FriendItem";
 
 const Nav = (props) => {
-    debugger;
     let friendsElements =
         props.friends.map ( f => <FriendItem friend={f.friend} />);
     return (
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,65 +1,58 @@
 import React from "react";
 import s from "./User.module.css";
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/15.png'
 
 const Users = (props) => {
-  debugger;
   if (props.users.length === 0) {
-    return props.setUsers([
-      {
-        id: 1,
-        photoUrl:
-          "https://images-na.ssl-images-amazon.com/images/I/91ErqttBmKL._RI_.jpg",
-        followed: false,
-        fullName: "Ivan",
-        status: "I am a boss",
-        location: { country: "Ukraine", city: "Odesa" },
-      },//u
-      {
-        id: 2,
-        photoUrl:
-          "https://images-na.ssl-images-amazon.com/images/I/91ErqttBmKL._RI_.jpg",
-        followed: true,
-        fullName: "Slava",
-        status: "I am a boss too",
-        location: { country: "Ukraine", city: "Sarata" },
-      },//u
-      {
-        id: 3,
-        photoUrl:
-          "https://images-na.ssl-images-amazon.com/images/I/91ErqttBmKL._RI_.jpg",
-        followed: false,
-        fullName: "Igor",
-        status: "I am a boss too",
-        location: { country: "Ukraine", city: "Vinnica" },
-      }//u
-    ])
-   }
-  debugger;
-  return <div> 
-        {props.users.map( u => <div className={s.users} key={u.id}>
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
+  }
+  return (
+    <div>
+      {props.users.map((u) => (
+        <div className={s.users} key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} />
+              <img src={u.photos.small != null ? u.photos.small : userPhoto} />
             </div>
             <div>
-              {u.followed
-                  ? <button onClick={ () => {props.follow(u.id)}}>Follow</button>
-                  : <button onClick={ () => {props.unfollow(u.id)}}>Unfollow</button>}
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  Follow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              )}
             </div>
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </span>
           </span>
         </div>
-      )}
+      ))}
     </div>
+  );
 };
 
 export default Users;
