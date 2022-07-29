7281867bde6567a6c71d4b894e1806fd3305d1ed
Kross55 <iyakushenko84@gmail.com>
2022-07-29 14:24:54 +0300

    lesson-88-pureFunction

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,9 +4,11 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-
+    
     let postsElements =
-        props.posts.map( p => <Post message={p.message} likes={p.likes}/>)
+        [...props.posts]//копируем пропсы, чтобы не мутировать стейт
+            .reverse()//мутирующий метод массива(чтобы его применить нужно предварительно скопировать массив)
+            .map( p => <Post message={p.message} likes={p.likes}/>)//не мутирующий метод массива
 
     return (
         <div className={s.myPosts}>
