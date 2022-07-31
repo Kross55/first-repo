3e0d2361d73d6a5ae0cde70add081513f52e166d
Kross55 <iyakushenko84@gmail.com>
2022-07-31 11:03:38 +0300

    lesson-90-Users-Paginator/User-final-refactor-3

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,16 +3,6 @@ import Paginator from "../common/Paginator/Paginator";
 import User from "./User";
 
 const Users = ({totalUsersCount, pageSize, currentPage, onPageChanged, ...props}) => {
-  /*
-  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-
-  let pages = [];
-  for (let i = 1; i <= pagesCount; i++) {
-    pages.push(i);
-    if (i === 20) break; //всего 3960 страниц, мы останавливаем for на 10-й странице
-  }
-  */
-
   return (
     <div>
       <Paginator
@@ -21,17 +11,6 @@ const Users = ({totalUsersCount, pageSize, currentPage, onPageChanged, ...props}
         currentPage={currentPage}
         onPageChanged={onPageChanged}
       />
-      {/*}
-      <div>
-        {pages.map((p) => {
-          return (
-            <span className={props.currentPage === p && styles.selectedPage}
-            onClick={(e) => {props.onPageChanged(p)}}>
-              {p}
-            </span>)})
-        }
-      </div>
-      */}
       <div>
         {props.users.map((u) => (
           <User
@@ -41,51 +20,6 @@ const Users = ({totalUsersCount, pageSize, currentPage, onPageChanged, ...props}
             unfollow={props.unfollow}
             follow={props.follow}
           />
-          /*
-        <div key={u.id}>
-          <span>
-            <div>
-              <NavLink to={"/profile/" + u.id}>
-                <img
-                  src={u.photos.small != null ? u.photos.small : userPhoto}
-                  className={styles.userPhoto}
-                />
-              </NavLink>
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  disabled={props.followingInProgress.some((id) => id === u.id)}
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  disabled={props.followingInProgress.some((id) => id === u.id)}
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
-            <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
-            </span>
-            <span>
-              <div>{"u.location.country"}</div>
-              <div>{"u.location.city"}</div>
-            </span>
-          </span>
-        </div>
-        */
         ))}
       </div>
     </div>
