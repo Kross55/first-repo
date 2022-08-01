3ec2d3f60a4f772bf0102c51d57af1283d577825
Kross55 <iyakushenko84@gmail.com>
2022-08-01 10:04:56 +0300

    lesson-91-redux-store-redux-devTools-setting

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
+import { applyMiddleware, combineReducers, legacy_createStore as createStore, compose } from "redux";
 import profileReducer from './profileReduser';
 import dialogsReducer from "./dialogsReduser";
 import sidebarReducer from "./sidebarReduser";
@@ -16,7 +16,8 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware) ) );
 
 window.store = store
 
