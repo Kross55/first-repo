7450a75e1b3b8647c60bde41536bf4a3e2a9e69b
Kross55 <iyakushenko84@gmail.com>
2022-08-06 11:07:21 +0300

    lesson-97-1-my-profile-with-button-Edit-to-start-Formic

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Preloader from "../../common/Preloader/Preloader";
 import s from "./ProfileInfo.module.css";
 import smile from '../../../assets/images/smile.png'
@@ -6,6 +6,13 @@ import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 import userPhoto from "../../../assets/images/user.png"
 
 const ProfileInfo = (props) => {
+
+    let [editMode, setEditMode] = useState(false)//используем локальный стейт для editMode
+
+    const activateEditMode = () => {//переменная для изменения editMode
+        setEditMode(true)
+    }
+
     if(!props.profile){
        return <Preloader />
     }
@@ -24,23 +31,68 @@ const ProfileInfo = (props) => {
             <div className={s.profileView}>
                 <img src={props.profile.photos.large || userPhoto}/>
                 {props.isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}{/*добавляем onChange чтобы можно было менять наше фото*/ }
-                <div>{props.profile.fullName}</div>
+                
                 <ProfileStatusWithHooks
                     status={props.status}
                     updateStatus={props.updateStatus} />
-                <div className={s.smile}>Ищу работу: {props.profile.lookingForAJob ? <img src={smile}/> : null}</div>
-                <div>Описание: {props.profile.lookingForAJobDescription}</div>
-                <div>Мои контакты:</div>
-                <div>
-                    <div><a href='' >{props.profile.contacts.facebook}</a></div>
-                    <div><a href='' >{props.profile.contacts.vk}</a></div>
-                    <div><a href='' >{props.profile.contacts.twitter}</a></div>
-                    <div><a href='' >{props.profile.contacts.instagram}</a></div>
-                    <div><a href='' >{props.profile.contacts.github}</a></div>
-                </div>
+                
+                { editMode 
+                   ? <ProfileDataForm profile={props.profile}/> //компонента для формы Formic где мы будем заполнять наши данные и отсылать на сервер
+                   : <ProfileData //компонент с нашими данными которые мы будем получать из сервера
+                        profile={props.profile} 
+                        isOwner={props.isOwner}
+                        activateEditMode={activateEditMode}/>}{/*можно обойтись и без переменной, прописать activateEditMode={ () => {setEditMode(true)} }*/} 
+
             </div>
         </div>
     )
+}
+
+const ProfileData = (props) => {
+    return (
+        <div>
+            {props.isOwner && <div><button onClick={props.activateEditMode}>Edit</button></div>}{/*кнопка для переключения между компонентами отображения данных и заполнения данных*/}
+            <div>
+                <b>Name</b>: {props.profile.fullName}
+            </div>
+            <div className={s.smile}>
+                <b>Lookig for a JOB</b>: {props.profile.lookingForAJob ? 'In active search' : <img src={smile} />}
+            </div>
+            {(props.profile.lookingForAJob &&
+                <div>
+                    <b>My profetional skills</b>: {props.profile.lookingForAJobDescription}
+                </div>) ||
+                <div>
+                    <b>Description</b>: I am playing with search!
+                </div>
+            }
+            <div>
+                <b>About me</b>: {props.profile.aboutMe}
+            </div>
+            <div>
+                <b>My contacts</b>: {Object.keys(props.profile.contacts).map(key =>  //Object.keys появился для обработки объекта contacts из нашего профиля, который мы будем мапить! 
+                    <Contacts key={key} contactTitle={key} contactValue={props.profile.contacts[key] || "empty"} />
+                )}
+            </div>
+            {/*
+            <div className={s.contacts}>  // это я доставал каждое значение ключа объекта contacts отдельно и отображал их в виде ссылок
+                <div><a href='' >{props.profile.contacts.facebook || <div>facebook.com</div> }</a></div>
+                <div><a href='' >{props.profile.contacts.twitter || <div>twitter.com</div> }</a></div>
+                <div><a href='' >{props.profile.contacts.instagram || <div>instagram.com</div> }</a></div>
+                <div><a href='' >{props.profile.contacts.github || <div>github.com</div> }</a></div>
+            </div>
+            */}
+        </div>
+    )
+}
+const ProfileDataForm = (props) => {
+    return (
+        <div>Formic</div>
+    )
+}
+
+const Contacts = ({contactTitle, contactValue}) => {
+    return <div className={s.contacts} ><b>{contactTitle}</b>: {contactValue}</div>  
 }
 
 export default ProfileInfo
